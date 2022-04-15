99007165ca343d8b982fc5293dbcd94c72c9aaaa
rostislavSLA <87258530+rostislavSLA@users.noreply.github.com>
2022-04-15 18:00:33 +0300

    user view

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from .serializers import UserSerializer
+import datetime
+import jwt
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+from .serializers import UserSerializer
 
 
 class RegisterView(APIView):
@@ -9,4 +13,53 @@ class RegisterView(APIView):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('Пользователь не найден')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Неверный пароль')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=300),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+
+        return response
+
+
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Не прошедший проверку подлинности!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Не прошедший проверку подлинности!')
+
+        user = User.objects.filter(id=payload['id']).first()
+
+        serializer = UserSerializer(user)
         return Response(serializer.data)
